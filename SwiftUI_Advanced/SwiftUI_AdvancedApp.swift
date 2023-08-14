9c49d075f8e65efff08dbf1e5e13afc27fc1e7dd
Kirill Streltsov <kirill.streltsov@7p-group.com>
2023-08-14 19:32:42 +0200

    Initial Commit

---
+++ SwiftUI_Advanced/SwiftUI_AdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AdvancedApp.swift
+//  SwiftUI_Advanced
+//
+//  Created by Streltsov, Kirill, SEVEN PRINCIPLES on 14.08.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
